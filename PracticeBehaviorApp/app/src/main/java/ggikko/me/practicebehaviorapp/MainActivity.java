a0246c16bb5bbdda5a0b78b42eceea8cf7e9443d
ggikko <ggikko@hotmail.com>
2016-10-09 02:00:50 +0900

    add base layout + snack

--- PracticeBehaviorApp/app/src/main/java/ggikko/me/practicebehaviorapp/MainActivity.java
+++ PracticeBehaviorApp/app/src/main/java/ggikko/me/practicebehaviorapp/MainActivity.java
@@ -1,13 +1,30 @@
 package ggikko.me.practicebehaviorapp;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.fab)
+    void callOnClick(View view){
+        switch (view.getId()){
+            case R.id.fab:
+                Snackbar.make(findViewById(R.id.container), "Hey there!", Snackbar.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
 }
